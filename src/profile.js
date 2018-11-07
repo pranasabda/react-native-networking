95e502c3d327290831590ca4a74f27e68df5acaa
pranasabda <pranasabda@gmail.com>
2018-11-07 17:40:49 +0700

    initial commit

---
+++ src/profile.js
@@ -0,0 +1,122 @@
+import React, {Component} from 'react';
+import {Text, View, ScrollView, ActivityIndicator } from 'react-native';
+
+export default class Profile extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            data:'',
+            dataProfile: [],
+            isLoading: true
+        }
+    }
+
+
+    componentDidMount = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'GET'
+    })
+    .then( (response) => response.json() )
+    .then(    
+        (json) => { console.log(json)
+            this.setState({
+                dataProfile: json,
+                isLoading: false
+            })
+        })
+    }
+
+    // {    
+    //     fetch('http://jsonplaceholder.typicode.com/users')
+    //         method: 'POST',
+    //         headers: {
+    //             Accept: 'application/json',
+    //             'Content-Type'
+    //         }
+    // }
+
+
+    render() {
+   
+        return (
+            <View style={{flex:1}}>
+
+            
+            { this.state.isLoading 
+            ?(
+                <View style={{flex:1}}> 
+                <ActivityIndicator/>
+                </View>
+            )
+            :(
+                <ScrollView style={{flex:1}}>
+                    {this.state.dataProfile.map( (d, i ) => {
+
+                        return (
+                            <View key={i}>
+                            <Text>{d.title}</Text>
+                            <Text>============================ {'\n'}</Text>
+                            </View>
+                        )
+
+                     })
+                    }
+                </ScrollView>
+            ) 
+            }
+
+            </View>
+        )
+    }
+}
+
+// [
+//     {
+//       "id": 1,
+//       "name": "Leanne Graham",
+//       "username": "Bret",
+//       "email": "Sincere@april.biz",
+//       "address": {
+//         "street": "Kulas Light",
+//         "suite": "Apt. 556",
+//         "city": "Gwenborough",
+//         "zipcode": "92998-3874",
+//         "geo": {
+//           "lat": "-37.3159",
+//           "lng": "81.1496"
+//         }
+//       },
+//       "phone": "1-770-736-8031 x56442",
+//       "website": "hildegard.org",
+//       "company": {
+//         "name": "Romaguera-Crona",
+//         "catchPhrase": "Multi-layered client-server neural-net",
+//         "bs": "harness real-time e-markets"
+//       }
+//     },
+
+//     {
+//       "id": 2,
+//       "name": "Ervin Howell",
+//       "username": "Antonette",
+//       "email": "Shanna@melissa.tv",
+//       "address": {
+//         "street": "Victor Plains",
+//         "suite": "Suite 879",
+//         "city": "Wisokyburgh",
+//         "zipcode": "90566-7771",
+//         "geo": {
+//           "lat": "-43.9509",
+//           "lng": "-34.4618"
+//         }
+//       },
+//       "phone": "010-692-6593 x09125",
+//       "website": "anastasia.net",
+//       "company": {
+//         "name": "Deckow-Crist",
+//         "catchPhrase": "Proactive didactic contingency",
+//         "bs": "synergize scalable supply-chains"
+//       }
+//     },
+// ]
